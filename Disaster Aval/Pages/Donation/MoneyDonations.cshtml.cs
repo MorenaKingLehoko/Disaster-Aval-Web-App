eea081348e912cfe3aa528ecee90008d1c87d3c9
Morena Lehoko <ST10127774@rcconnect.edu.za>
2023-10-19 00:26:45 +0000

    Changed heading of MoneyDonation to make it clear they allocating money

--- Disaster Aval/Pages/Donation/MoneyDonations.cshtml.cs
+++ Disaster Aval/Pages/Donation/MoneyDonations.cshtml.cs
@@ -16,7 +16,7 @@ namespace Disaster_Aval.Pages.Donation
         [DisplayName("UserID To Remain Anonymous")]
         [Required]
         public int UserID { get; set; }
-        [DisplayName("Allocate Money to an Actie Disatser (Specify by ID)")]
+        [DisplayName("Allocate Money to Disatser, (Specify by DisasterID)")]
         [Required]
         public int DisasterID { get; set; }
         [DisplayName("How Much Are you donating R:")]
