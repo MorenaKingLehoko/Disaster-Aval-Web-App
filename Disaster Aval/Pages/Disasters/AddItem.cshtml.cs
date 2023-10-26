cc51aeb14606693d5a43da45a15909df9f25b291
Morena Lehoko <ST10127774@rcconnect.edu.za>
2023-10-26 22:11:14 +0000

    Redireting the user to the Update page, after they descibed their item

--- Disaster Aval/Pages/Disasters/AddItem.cshtml.cs
+++ Disaster Aval/Pages/Disasters/AddItem.cshtml.cs
@@ -102,7 +102,7 @@ namespace Disaster_Aval.Pages.Donation
                 conn.Close();
 
                 // Redirecting to the success page or handle success as needed
-                return RedirectToPage("DonationSuccess");
+                return RedirectToPage("UpdatePage");
             }
         }
         //catch (Exception ex)
