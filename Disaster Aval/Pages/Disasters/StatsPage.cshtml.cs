a3b84da07bd53358471b196c92bfa7b126aa90c5
Morena Lehoko <ST10127774@rcconnect.edu.za>
2023-11-23 16:35:46 +0000

    added a new page and a button to show the stats

---
+++ Disaster Aval/Pages/Disasters/StatsPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Disaster_Aval.Pages.Disasters
+{
+    public class StatsPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
