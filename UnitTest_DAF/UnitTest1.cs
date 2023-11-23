2cb018f16281dbf72fa3f09ae7b3e4425e0cf9db
Morena Lehoko <ST10127774@rcconnect.edu.za>
2023-11-23 23:29:38 +0000

    added unit testing for the adminHome page

---
+++ UnitTest_DAF/UnitTest1.cs
@@ -0,0 +1,27 @@
+using Disaster_Aval.Pages.Disasters;
+using Moq;
+
+namespace UnitTest_DAF
+{
+    [TestClass]
+    public class AdminHomeModelTests
+    {
+        [TestMethod]
+        public void OnGet_ShouldRetrieveActiveDisasters()
+        {
+            // Arrange
+            var adminServiceMock = new Mock<IAdminService>();
+            adminServiceMock.Setup(service => service.GetActiveDisasters()).Returns(new List<string> { "MockDisaster1", "MockDisaster2" });
+
+            var pageModel = new AdminHomeModel(adminServiceMock.Object);
+
+            // Act
+            pageModel.OnGet();
+
+            // Assert
+            Assert.IsNotNull(pageModel.ActiveDisasters);
+            Assert.AreEqual(2, pageModel.ActiveDisasters.Count);
+            // Add more assertions based on your specific requirements
+        }
+    }
+}
