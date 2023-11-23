4732a114c2e3274a0d2f379a2918297cff95da10
Morena Lehoko <ST10127774@rcconnect.edu.za>
2023-11-23 16:57:55 +0000

    Added a new page to show  the total Money

---
+++ Disaster Aval/Pages/Stats/TotalMoney.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Disaster_Aval.Pages.Stats
+{
+    public class TotalMoneyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
