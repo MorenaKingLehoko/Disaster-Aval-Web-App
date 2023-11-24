55cd30080e280a7f75869beb8dcab52e703c4e35
Morena Lehoko <ST10127774@rcconnect.edu.za>
2023-11-24 08:09:27 +0000

    added a new unit test 'OnPost_ShouldRetrieveValuesFromForm'

--- UnitTest_DAF/UnitTest1.cs
+++ UnitTest_DAF/UnitTest1.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using Xunit;
+using Disaster_Aval.Pages.Login;
+using Microsoft.Extensions.Primitives;
 
 namespace UnitTest_DAF
 {
@@ -28,9 +30,46 @@ namespace UnitTest_DAF
             // Add more assertions based on your specific requirements
         }
     }
+    [TestClass]
+    public class AdminLoginModelTests
+    {
+        [TestMethod]
+        public void OnPost_ShouldRetrieveValuesFromForm()
+        {
+            // Arrange
+            var model = new AdminLoginModel();
+            var context = new DefaultHttpContext();
+            model.PageContext = new PageContext { HttpContext = context };
 
+            // Simulate form values
+            context.Request.Form = new FormCollection(new Dictionary<string, StringValues>
+            {
+                { "AdminEmail", "test@example.com" },
+                { "AdminPassword", "password123" },
+                { "AdminName", "John" },
+                { "AdminSurname", "Doe" }
+                // Add other form values as needed
+            });
 
+            // Act
+            var result = model.OnPost();
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(RedirectToPageResult));
+            Assert.AreEqual("YourExpectedPage", (result as RedirectToPageResult)?.PageName);  // Change "YourExpectedPage" to the expected page name
+
+            // Add more assertions based on your specific requirements
+            Assert.AreEqual("test@example.com", model.AdminEmail);
+            Assert.AreEqual("password123", model.AdminPassword);
+            Assert.AreEqual("John", model.AdminName);
+            Assert.AreEqual("Doe", model.AdminSurname);
+            // Add assertions for other properties as needed
+        }
+    }
 }
+    
+    
+
 
 
 
